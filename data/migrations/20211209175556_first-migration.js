34c7f93865559998ae68ce29578c8e1739481ecf
Ladrillo <gabriel@cabrejas.net>
2021-12-09 18:56:57 +0100

    committing to "lecture"

--- data/migrations/20211209175556_first-migration.js
+++ data/migrations/20211209175556_first-migration.js
@@ -1,8 +1,14 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+  await knex.schema
+    .createTable()
+    .createTable()
+    .createTable()
+    .createTable()
+
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.
+
 };
