29da7117f32f0de1dc0eec0bcdf8017c8c57c687
Ladrillo <gabriel@cabrejas.net>
2021-12-09 19:00:47 +0100

    committing to "lecture"

--- data/migrations/20211209175556_first-migration.js
+++ data/migrations/20211209175556_first-migration.js
@@ -9,8 +9,8 @@ exports.up = async function(knex) {
 
 exports.down = async function(knex) {
   await knex.schema
-    .dropTableIfExists()
-    .dropTableIfExists()
+    .dropTableIfExists('zoo_animals')
+    .dropTableIfExists('animals')
     .dropTableIfExists('species')
     .dropTableIfExists('zoos')
 }
