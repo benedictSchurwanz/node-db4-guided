7eb7410716a61674be74abefd0d738c82718126e
Ladrillo <gabriel@cabrejas.net>
2021-12-09 19:13:54 +0100

    committing to "lecture"

--- data/migrations/20211209175556_first-migration.js
+++ data/migrations/20211209175556_first-migration.js
@@ -4,15 +4,15 @@ exports.up = async function(knex) {
     .createTable('zoos', table => {
       table.increments('zoo_id')
       table.string('zoo_name', 128).notNullable()
-      table.string('zoo_address', 256).
+      table.string('zoo_address', 256).notNullable().unique()
     })
     .createTable('species', table => {
       table.increments('species_id')
-      table.string('species_name', 128)
+      table.string('species_name', 128).notNullable()
     })
     .createTable('animals', table => {
       table.increments('animal_id')
-      table.string('animal_name', 128)
+      table.string('animal_name', 128).notNullable()
     })
     .createTable('zoo_animals', table => {
       table.increments('zoo_animal_id')
