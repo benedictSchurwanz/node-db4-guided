1e1bf6e05654d944da419496700f373272ef993d
Ladrillo <gabriel@cabrejas.net>
2021-12-09 18:59:46 +0100

    committing to "lecture"

--- data/migrations/20211209175556_first-migration.js
+++ data/migrations/20211209175556_first-migration.js
@@ -2,7 +2,7 @@
 exports.up = async function(knex) {
   await knex.schema
     .createTable('zoos')
-    .createTable('animals')
+    .createTable('species')
     .createTable()
     .createTable()
 }
